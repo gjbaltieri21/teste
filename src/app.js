6c07bce7f2d26af0fb610c26f7a76e9a0ca5dcc4
gjbaltieri21 <111377485+gjbaltieri21@users.noreply.github.com>
2022-08-22 11:33:49 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -4,18 +4,8 @@ const ejs = require( 'ejs')
 const vision = require( '@hapi/vision')
 const path = require( 'path')
 
-const cluster = require('cluster');
-const numCPUs = require('os').cpus().length;
-
-if (cluster.isMaster) {
-  console.log('Master process is running');
-  // Fork workers
-  for (let i = 0; i < numCPUs; i++) {
-    cluster.fork();
-  }
-} else {
   const app = new Hapi.server({
-  port: 7000 || process.env.PORT})
+  port: process.env.PORT || 7000})
 
   async function main() {
     await app.register(vision)
@@ -32,20 +22,3 @@ if (cluster.isMaster) {
     return app
   }
   main()
-}
-
-// async function main() {
-//   await app.register(vision)
-//   app.views({
-//     engines: {
-//       ejs: ejs,
-//     },
-//     relativeTo: path.join(__dirname),
-//     path: './templates',
-//   })
-//   app.route([new routes().home(), new routes().update()])
-//   await app.start()
-//   console.log("Connectado ao servidor...")
-//   return app
-// }
-// module.exports = main()
